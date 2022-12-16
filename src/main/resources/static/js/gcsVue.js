9b8481bd61a3a4bdecf77796782cc6355d99625c
Duckhwa Song <kf80song@gmail.com>
2022-12-16 16:13:06 +0900

    vue test

--- src/main/resources/static/js/gcsVue.js
+++ src/main/resources/static/js/gcsVue.js
@@ -2,3 +2,35 @@ let gcsVue={
 	data:{},	
 	methods: {}
 };
+gcsVue.methods.getDataGET=function(url,callback){
+        fetch(url)
+        .then(response=>response.json())
+        .then(json=>{
+			gcsVue.data.result = json			
+			//callback();
+			})
+        .catch(error=>{
+            console.log(error);
+        });
+    };
+gcsVue.methods.getDataPOST=function(){
+        const data = {
+            'got': 'hello~ world! 한글'
+        }
+
+        const option = {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            //body: JSON.stringify(data)
+        }
+
+        fetch('https://devcms.firstmall.kr/boarddata/catalog?board_seq=1&mn=14', option)
+        .then(response=>response.json())
+        .then(json=>this.students2 = json)
+        .catch(error=>{
+            console.log(error);
+        });	
+	};
