593b71fceb8a4f7d1269cfe6e9df41af8af5a5ae
Duckhwa Song <kf80song@gmail.com>
2022-12-19 00:06:16 +0900

    vue 테스트

--- src/main/resources/static/js/gcsVue.js
+++ src/main/resources/static/js/gcsVue.js
@@ -13,6 +13,38 @@ gcsVue.methods.getDataGET=function(url,callback){
             console.log(error);
         });
     };
+    
+async function getData(url){
+  let response = fetch(url);
+  return response;
+}
+
+async function $api(url, method, data) {
+        return (
+          await axios({
+            method: method,
+            url,
+            data,
+          }).catch((e) => {
+            console.log(e);
+          })
+        ).data;
+      }
+
+async function $apiGet(url) {
+        return (
+          await axios.get(url).catch((e) => {
+            console.log(e);
+          })
+        ).data;
+      }
+async function axiosGet(url){
+        //this.jsonData.axData = await this.$api("https://api.github.com/users/hadley/orgs","get");
+        axiosData = await this.$apiGet(url);
+        return axiosData;
+      }
+
+
 gcsVue.methods.getDataPOST=function(){
         const data = {
             'got': 'hello~ world! 한글'
